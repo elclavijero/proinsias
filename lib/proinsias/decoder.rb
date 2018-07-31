4abfa0bd277d0f7949f39ee29205ec0e277136a0
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-07-31 16:01:00 +0100

    First passing test

--- lib/proinsias/decoder.rb
+++ lib/proinsias/decoder.rb
@@ -1,6 +1,21 @@
+require 'moory'
+
 module Proinsias
   module Decoder
-    class Automaton
+    RULES = """
+    ^ : p / variable / produce : ^
+    """
+    
+    class Automaton < Moory::Logistic::Unit
+      def initialize(consumer)
+        @consumer = consumer
+        super(rules: RULES)
+        repertoire.learn(name: 'produce', item: method(:produce))
+      end
+
+      def produce(output)
+        @consumer.call(output)
+      end
     end
   end
 end
--- spec/decoder_spec.rb
+++ spec/decoder_spec.rb
@@ -1,11 +1,19 @@
 RSpec.describe Proinsias::Decoder::Automaton do
-  let(:the_automaton) do
-    Proinsias::Decoder::Automaton.new
+  let(:the_decoder) do
+    Proinsias::Decoder::Automaton.new(the_consumer)
+  end
+
+  let(:the_consumer) do
+    spy("the consumer")
   end
 
   describe '#issue' do
     context 'producing a variable' do
-      it 'can be achieved by issuing the character "p"'
+      it 'can be achieved by issuing the character "p"' do
+        the_decoder.issue('p')
+
+        expect(the_consumer).to have_received(:call).with('variable')
+      end
 
       it 'can be achieved by issuing the character "q"'
 
