df9b9388e05dd642878f613023af2f625e9a0bd3
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-08-01 22:25:45 +0100

    join left vacancy

--- lib/proinsias/assembler.rb
+++ lib/proinsias/assembler.rb
@@ -2,6 +2,10 @@ module Proinsias
   class Assembler
     def Assembler.join(left:, right:)
       return right if left.nil?
+
+      if vacancy = left.vacancy
+        vacancy.receive(right)
+      end
     end
   end
 end
--- spec/assembler_spec.rb
+++ spec/assembler_spec.rb
@@ -17,5 +17,33 @@ RSpec.describe Proinsias::Assembler do
         )
       end
     end
+
+    context 'when left is not nil' do
+      let(:left) do
+        spy("left")
+      end
+
+      let(:right) do
+        spy("right")
+      end
+
+      context 'and left offers a vacancy' do
+        let(:a_vacancy) do
+          spy("a vacancy")
+        end
+
+        before do
+          allow(left).to receive(:vacancy).and_return(a_vacancy)
+        end
+
+        it 'will ask left to receive right' do
+          Proinsias::Assembler.join(left: left, right: right)
+
+          expect(a_vacancy).to have_received(:receive).with(right)
+        end
+      end
+
+      context 'and left offers no vacancy'
+    end
   end
 end
