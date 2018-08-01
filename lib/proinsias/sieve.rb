e5b13ce494a4cb56b35d45c8fdb2984c0fef2ae2
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-08-01 14:05:43 +0100

    Trivial case for Sieve::Unit#issue

--- lib/proinsias/sieve.rb
+++ lib/proinsias/sieve.rb
@@ -1,4 +1,56 @@
+require 'moory'
+
 module Proinsias
-  class Sieve
+  module Sieve
+    CONFIG = {
+      basis: 'basis',
+      specs: {
+        'basis' => {
+          rules: """
+          ^ : constant : $
+          ^ : variable : $
+          ^ : prefix / open          / defer : Δ
+          ^ : (      / parenthetical / defer : Δ
+          $ : infix  / open          / defer : Δ
+          
+          Δ : term : $
+          """,
+        },
+        'open' => {
+          rules: """
+          ^ : constant / term          / reconvene : $
+          ^ : variable / term          / reconvene : $
+          ^ : prefix   / open          / defer : Δ
+          ^ : (        / parenthetical / defer : Δ
+          $ : infix    / open          / defer : Δ
+          
+          Δ : term     / term          / reconvene : $
+          """,
+        },
+        'parenthetical' => {
+          rules: """
+          ^ : constant : C
+          ^ : variable : C
+          ^ : prefix / open          / defer : Δ
+          ^ : (      / parenthetical / defer : Δ
+          ^ : )      / void          / reconvene : $
+          C : infix  / open          / defer : Δ
+          C : )      / term          / reconvene : $
+          
+          Δ : term : C
+          """
+        }
+      }
+    }
+
+    class Unit
+      def initialize(consumer)
+        @consumer = consumer
+      end
+
+      def issue(role:, glyph:)
+        @consumer.call(role: role, glyph: glyph)
+      end
+    end
   end
 end
--- spec/sieve_spec.rb
+++ spec/sieve_spec.rb
@@ -1,2 +1,25 @@
-RSpec.describe Proinsias::Sieve do
+RSpec.describe Proinsias::Sieve::Unit do
+  let(:the_sieve) do
+    Proinsias::Sieve::Unit.new(the_consumer)
+  end
+
+  let(:the_consumer) do
+    spy("the consumer")
+  end
+
+  describe '#issue' do
+    describe 'issuing some simple token sequences' do
+      context 'when given a token whose role is "constant"' do
+        let(:constant_token) do
+          { role: 'constant', glyph: 'true' }
+        end
+        
+        it 'will pass that token to the consumer' do
+          the_sieve.issue(constant_token)
+
+          expect(the_consumer).to have_received(:call).with(constant_token)
+        end
+      end
+    end
+  end
 end
