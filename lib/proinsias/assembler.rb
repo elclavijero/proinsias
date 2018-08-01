11e0cecc9e4ddd1e9c1fd7ee7af18f16d8451468
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-08-01 22:18:18 +0100

    Assembler.join basis

--- lib/proinsias/assembler.rb
+++ lib/proinsias/assembler.rb
@@ -1,5 +1,7 @@
 module Proinsias
   class Assembler
-    def Assembler.join;end
+    def Assembler.join(left:, right:)
+      return right if left.nil?
+    end
   end
 end
--- spec/assembler_spec.rb
+++ spec/assembler_spec.rb
@@ -4,4 +4,18 @@ RSpec.describe Proinsias::Assembler do
       expect(Proinsias::Assembler).to respond_to(:join)
     end
   end
+
+  describe 'Assembler.join' do
+    context 'when left is nil' do
+      it 'will return right' do
+        some_object = spy("some object")
+
+        expect(
+          Proinsias::Assembler.join(left: nil, right: some_object)
+        ).to equal(
+          some_object
+        )
+      end
+    end
+  end
 end
