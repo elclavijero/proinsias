c5fc8dfdff22af5e5e09cf1ccc16d1ed2424c1a6
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-08-01 23:01:41 +0100

    Some Atoms

--- lib/proinsias/atoms.rb
+++ lib/proinsias/atoms.rb
@@ -1,6 +1,39 @@
 module Proinsias
   module Atoms
     class Variable
+      include Proinsias::Receiver
+
+      def initialize(glyph)
+        @glyph = glyph
+        @capacity = 0
+      end
+    end
+
+    class Constant
+      include Proinsias::Receiver
+
+      def initialize(glyph)
+        @glyph = glyph
+        @capacity = 0
+      end
+    end
+
+    class Prefix
+      include Proinsias::Receiver
+
+      def initialize(glyph)
+        @glyph = glyph
+        @capacity = 1
+      end
+    end
+
+    class Infix
+      include Proinsias::Receiver
+
+      def initialize(glyph)
+        @glyph = glyph
+        @capacity = 2
+      end
     end
   end
 end
