f2d778c65e6fd2d80054792a142d6a97001d0f48
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-08-01 22:14:03 +0100

    class method in place

--- lib/proinsias/assembler.rb
+++ lib/proinsias/assembler.rb
@@ -1,4 +1,5 @@
 module Proinsias
   class Assembler
+    def Assembler.join;end
   end
 end
--- spec/assembler_spec.rb
+++ spec/assembler_spec.rb
@@ -1,2 +1,7 @@
 RSpec.describe Proinsias::Assembler do
+  describe 'its interface' do
+    it 'exposes join' do
+      expect(Proinsias::Assembler).to respond_to(:join)
+    end
+  end
 end
