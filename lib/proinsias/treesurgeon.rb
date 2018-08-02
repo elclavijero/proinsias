77cf716c7c69b11bc79b660bc1b54414e8916dac
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-08-02 23:14:32 +0100

    Corrected method invocation

--- lib/proinsias/treesurgeon.rb
+++ lib/proinsias/treesurgeon.rb
@@ -2,6 +2,25 @@ module Proinsias
   class TreeSurgeon
     def TreeSurgeon.join(stock:, scion:)
       return scion if stock.nil?
+
+      if scion.full?
+        vacancy = stock.seek(Inspectors[:vacancy])
+        vacancy.receive(scion)
+        return stock
+      end
+
+      if scion.expectant?
+        if stock >= scion
+          scion.receive(left)
+          return scion
+        else
+          seam = stock.seek(Inspectors[:seam][left])
+          cutting = cleave(seam)
+          scion.receive(cutting.scion)
+          cutting.stock.receive(scion)
+          return stock
+        end
+      end
     end
 
     def TreeSurgeon.cleave(stock)
