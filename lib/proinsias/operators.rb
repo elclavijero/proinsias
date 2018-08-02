422fbe11fcdf6c6211f7560108ce57a6600b47eb
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-08-02 22:02:51 +0100

    Some operators

--- lib/proinsias/operators.rb
+++ lib/proinsias/operators.rb
@@ -1,12 +1,38 @@
 module Proinsias
   module Operators
     class Equivalence < Atoms::Infix
+      attr_reader :precedence
+
       def initialize
         super('≡')
+        @precedence = 0
       end
+    end
 
-      def precedence
-        0
+    class Negation < Atoms::Prefix
+      attr_reader :precedence
+
+      def initialize
+        super('¬')
+        @precedence = 10
+      end
+    end
+
+    class Disjunction < Atoms::Infix
+      attr_reader :precedence
+
+      def initialize
+        super('∨')
+        @precedence = 2
+      end
+    end
+
+    class Conjunction < Atoms::Infix
+      attr_reader :precedence
+
+      def initialize
+        super('∧')
+        @precedence = 2
       end
     end
   end
--- spec/operators_spec.rb
+++ spec/operators_spec.rb
@@ -1,11 +1,17 @@
-RSpec.describe Proinsias::Operators do
-  let(:a_connective) do
+RSpec.describe Proinsias::Operators::Equivalence do
+  let(:an_equivalence) do
     Proinsias::Operators::Equivalence.new
   end
 
   describe 'its interface' do
     it 'responds to #precedence' do
-      expect(a_connective).to respond_to(:precedence)
+      expect(an_equivalence).to respond_to(:precedence)
+    end
+  end
+
+  describe '#precedence' do
+    it 'will return 0' do
+      expect(an_equivalence.precedence).to eq(0)
     end
   end
 end
