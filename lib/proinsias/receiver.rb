5080e012061550fc7606ef0c7c1aa6d89bc58b28
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-08-05 17:28:49 +0100

    Removed pointless methods

--- lib/proinsias/receiver.rb
+++ lib/proinsias/receiver.rb
@@ -26,46 +26,6 @@ module Proinsias
       received.last
     end
 
-    def seek(inspector)
-      inspector.call(self) ?
-        self :
-        refer(inspector)
-    end
-
-    def step_up(mark)
-      seek(
-        proc { |candidate| candidate.received.last >= mark }
-      )
-    end
-
-    def integrate(incoming)
-      if self < incoming
-        unplug(step_up(incoming)).tap do |unplugged|
-          incoming.receive(unplugged.plug)
-          unplugged.receiver.receive(incoming)
-        end
-        self
-      elsif self == incoming
-        # This is where we will have to consider "contrary" receivers
-        # which we don't yet have.
-        incoming.receive(self)
-        incoming
-      elsif self > incoming
-        incoming.receive(self)
-        incoming
-      end
-    end
-    
-    def unplug(stock)
-      Unplugged.new(
-        receiver: stock,
-        plug:     stock.nodes.pop
-      )
-    end
-
-    Unplugged = Struct.new(:receiver, :plug, keyword_init: true)
-
-    # new interface
     def accommodates?(other)
       other > last
     end
@@ -79,14 +39,6 @@ module Proinsias
     def splice(other)
       other.receive(received.pop)
       receive(other)
-    end
-
-    private
-    
-    def refer(inspector)
-      received.count > 0 ?
-        received.last.seek(inspector) :
-        nil
     end
   end
 end
