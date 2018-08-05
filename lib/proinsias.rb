b53c7a9f3b94c5d266354f3d81bd74bb7d9056a8
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-08-05 12:59:25 +0100

    Pruning

--- lib/proinsias.rb
+++ lib/proinsias.rb
@@ -4,7 +4,6 @@ require "proinsias/decoder"
 require "proinsias/configurations"
 require "proinsias/sieve"
 require "proinsias/receiver"
-require "proinsias/treesurgeon"
 require "proinsias/syntacticroles"
 require "proinsias/operators"
 
