446637ceef0936d9d0fa456fc1f92a45924fd641
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-08-06 20:06:13 +0100

    First characterisation

--- spec/assembler_spec.rb
+++ spec/assembler_spec.rb
@@ -1,2 +1,11 @@
 RSpec.describe Proinsias::Assembler do
+  let(:the_assembler) do
+    Proinsias::Assembler.new
+  end
+
+  describe 'its interface' do
+    it 'exposes #feed' do
+      expect(the_assembler).to respond_to(:feed)
+    end
+  end
 end
