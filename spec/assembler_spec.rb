d47df37083f93855dc71b7001e5fc92b661369aa
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-08-07 16:29:18 +0100

    OK

--- spec/assembler_spec.rb
+++ spec/assembler_spec.rb
@@ -13,72 +13,74 @@ RSpec.describe Proinsias::Assembler do
     end
   end
 
-  describe 'Precedence assembly' do
-    before do
-      the_assembler.feed(Proinsias::Atoms::Variable.new('p'))
-      the_assembler.feed(Proinsias::Operators::Equivalence.new)
-      the_assembler.feed(Proinsias::Atoms::Variable.new('q'))
-      the_assembler.feed(Proinsias::Operators::Disjunction.new)
-      the_assembler.feed(Proinsias::Atoms::Variable.new('r'))
-      the_assembler.feed(Proinsias::Operators::Equality.new)
-      the_assembler.feed(Proinsias::Atoms::Variable.new('e'))
-    end
-
-    it 'will assemble the elements into a proper hierarchy' do
-      expect(the_assembler.receiver.to_ast).to eq(
-        {"≡"=>["p", {"∨"=>["q", {"="=>["r", "e"]}]}]}
-      )
-    end
-  end
-
-  describe 'Mixed fixity and arity assembly' do
-    before do
-      the_assembler.feed(Proinsias::Operators::Negation.new)
-      the_assembler.feed(Proinsias::Atoms::Variable.new('p'))
-      the_assembler.feed(Proinsias::Operators::Conjunction.new)
-      the_assembler.feed(Proinsias::Operators::Negation.new)
-      the_assembler.feed(Proinsias::Atoms::Variable.new('q'))
+  describe 'Assembling hierarchies from Atoms and Operators ' do
+    describe 'Precedence assembly' do
+      before do
+        the_assembler.feed(Proinsias::Atoms::Variable.new('p'))
+        the_assembler.feed(Proinsias::Operators::Equivalence.new)
+        the_assembler.feed(Proinsias::Atoms::Variable.new('q'))
+        the_assembler.feed(Proinsias::Operators::Disjunction.new)
+        the_assembler.feed(Proinsias::Atoms::Variable.new('r'))
+        the_assembler.feed(Proinsias::Operators::Equality.new)
+        the_assembler.feed(Proinsias::Atoms::Variable.new('e'))
+      end
   
-      the_assembler.feed(Proinsias::Operators::Equivalence.new)
-  
-      the_assembler.feed(Proinsias::Operators::Negation.new)
-      the_assembler.feed(Proinsias::Operators::Negation.new)
-      the_assembler.feed(Proinsias::Operators::Negation.new)
-      the_assembler.feed(Proinsias::Atoms::Variable.new('p'))
-      the_assembler.feed(Proinsias::Operators::Conjunction.new)
-      the_assembler.feed(Proinsias::Operators::Negation.new)
-      the_assembler.feed(Proinsias::Operators::Negation.new)
-      the_assembler.feed(Proinsias::Operators::Negation.new)
-      the_assembler.feed(Proinsias::Atoms::Variable.new('q'))
+      it 'will assemble the elements into a proper hierarchy' do
+        expect(the_assembler.receiver.to_ast).to eq(
+          {"≡"=>["p", {"∨"=>["q", {"="=>["r", "e"]}]}]}
+        )
+      end
     end
-
+  
+    describe 'Mixed fixity and arity assembly' do
+      before do
+        the_assembler.feed(Proinsias::Operators::Negation.new)
+        the_assembler.feed(Proinsias::Atoms::Variable.new('p'))
+        the_assembler.feed(Proinsias::Operators::Conjunction.new)
+        the_assembler.feed(Proinsias::Operators::Negation.new)
+        the_assembler.feed(Proinsias::Atoms::Variable.new('q'))
     
-    it 'will assemble the elements into a proper hierarchy' do
-      expect(the_assembler.receiver.to_ast).to eq(
-        {"≡"=>[
-            {"∧"=>[{"¬"=>["p"]}, {"¬"=>["q"]}]}, 
-            {"∧"=>[{"¬"=>[{"¬"=>[{"¬"=>["p"]}]}]}, {"¬"=>[{"¬"=>[{"¬"=>["q"]}]}]}]}
-          ]
-        }
-      )
+        the_assembler.feed(Proinsias::Operators::Equivalence.new)
+    
+        the_assembler.feed(Proinsias::Operators::Negation.new)
+        the_assembler.feed(Proinsias::Operators::Negation.new)
+        the_assembler.feed(Proinsias::Operators::Negation.new)
+        the_assembler.feed(Proinsias::Atoms::Variable.new('p'))
+        the_assembler.feed(Proinsias::Operators::Conjunction.new)
+        the_assembler.feed(Proinsias::Operators::Negation.new)
+        the_assembler.feed(Proinsias::Operators::Negation.new)
+        the_assembler.feed(Proinsias::Operators::Negation.new)
+        the_assembler.feed(Proinsias::Atoms::Variable.new('q'))
+      end
+  
+      
+      it 'will assemble the elements into a proper hierarchy' do
+        expect(the_assembler.receiver.to_ast).to eq(
+          {"≡"=>[
+              {"∧"=>[{"¬"=>["p"]}, {"¬"=>["q"]}]}, 
+              {"∧"=>[{"¬"=>[{"¬"=>[{"¬"=>["p"]}]}]}, {"¬"=>[{"¬"=>[{"¬"=>["q"]}]}]}]}
+            ]
+          }
+        )
+      end
     end
-  end
-
-  describe 'Right associativity' do
-    before do
-      the_assembler.feed(Proinsias::Atoms::Variable.new('p'))
-      the_assembler.feed(Proinsias::Operators::Implication.new)
-      the_assembler.feed(Proinsias::Atoms::Variable.new('q'))
-      the_assembler.feed(Proinsias::Operators::Implication.new)
-      the_assembler.feed(Proinsias::Atoms::Variable.new('r'))
-      the_assembler.feed(Proinsias::Operators::Implication.new)
-      the_assembler.feed(Proinsias::Atoms::Variable.new('s'))
-    end
-
-    it 'will assemble the elements into a proper hierarchy' do
-      expect(the_assembler.receiver.to_ast).to eq(
-        {"⇒"=>["p", {"⇒"=>["q", {"⇒"=>["r", "s"]}]}]}
-      )
+  
+    describe 'Right associativity' do
+      before do
+        the_assembler.feed(Proinsias::Atoms::Variable.new('p'))
+        the_assembler.feed(Proinsias::Operators::Implication.new)
+        the_assembler.feed(Proinsias::Atoms::Variable.new('q'))
+        the_assembler.feed(Proinsias::Operators::Implication.new)
+        the_assembler.feed(Proinsias::Atoms::Variable.new('r'))
+        the_assembler.feed(Proinsias::Operators::Implication.new)
+        the_assembler.feed(Proinsias::Atoms::Variable.new('s'))
+      end
+  
+      it 'will assemble the elements into a proper hierarchy' do
+        expect(the_assembler.receiver.to_ast).to eq(
+          {"⇒"=>["p", {"⇒"=>["q", {"⇒"=>["r", "s"]}]}]}
+        )
+      end
     end
   end
 end
