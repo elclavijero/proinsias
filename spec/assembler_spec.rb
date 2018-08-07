9b5d062c451fae071811826b85960188f5731ebb
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-08-07 16:16:53 +0100

    OK

--- spec/assembler_spec.rb
+++ spec/assembler_spec.rb
@@ -12,4 +12,25 @@ RSpec.describe Proinsias::Assembler do
       expect(the_assembler).to respond_to(:connect)
     end
   end
+
+  describe 'Fixity and precedence assembly' do
+    let(:eqv) { Proinsias::Operators::Equivalence.new }
+    let(:equ) { Proinsias::Operators::Equality.new    }
+    let(:neg) { Proinsias::Operators::Negation.new    }
+    let(:dis) { Proinsias::Operators::Disjunction.new }
+    let(:p)   { Proinsias::Atoms::Variable.new('p')   }
+    let(:q)   { Proinsias::Atoms::Variable.new('q')   }
+    let(:r)   { Proinsias::Atoms::Variable.new('r')   }
+    let(:e)   { Proinsias::Atoms::Variable.new('e')   }
+
+    it 'will ...' do
+      [ p, eqv, q, dis, r, equ, e ].each do |x|
+        the_assembler.feed(x)
+      end
+
+      expect(the_assembler.receiver.to_ast).to eq(
+        {"≡"=>["p", {"∨"=>["q", {"="=>["r", "e"]}]}]}
+      )
+    end
+  end
 end
