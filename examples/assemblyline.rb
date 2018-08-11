2bca47b8faf146051d24b39263953b193671acc5
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-08-11 14:20:22 +0100

    OK

--- examples/assemblyline.rb
+++ examples/assemblyline.rb
@@ -1,15 +1,15 @@
-line = Proinsias::AssemblyLine.new
+assembly_line = Proinsias::AssemblyLine.new
 
 director = Proinsias::Director.new(
-  consumer: line.method(:issue)
+  consumer: assembly_line.method(:issue)
 )
 
 scanner = Proinsias::Scanner.new(
   consumer: director.method(:issue)
 )
 
-"p ≡ q ∨ r = s".each_char do |c|
+"p ⇒ q ⇒ r ⇒ s".each_char do |c|
   scanner.issue(c)
 end
 
-pp line.product.to_ast
+pp assembly_line.product.to_ast
