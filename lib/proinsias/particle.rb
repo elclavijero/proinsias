8a92347c65ee360c9d652828343d6f7a832d6141
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-08-13 14:33:39 +0100

    Reshuffling

--- lib/proinsias/particle.rb
+++ lib/proinsias/particle.rb
@@ -1,28 +1,30 @@
 module Proinsias
   module Particle
+    def Particle.from_glyph(glyph)
+      Fundamental.new( glyph_properties(glyph) )
+    end
+
+    def Particle.glyph_properties(glyph)
+      DEFINITIONS.detect { |p| p[:glyph] == glyph }
+    end
+
     module Foundation
       include Receiver
       attr_reader :role
     end
-  end
-
-  module Particle
+    
     module Atom
       include Foundation
       include AST::Nonparous
       include Disposition::Pessimistic
     end
-  end
 
-  module Particle
     module Operator
       include Foundation
       include AST::Parous
       alias   arguments received
     end
-  end
 
-  module Particle
     module Outfix
       def receive(particle)
         if particle.role == @sentinel
@@ -32,16 +34,6 @@ module Proinsias
           fail "Particle is not the expected sentinel.  We wanted: #{@sentinel}"
         end
       end
-    end
-  end
-
-  module Particle
-    def Particle.from_glyph(glyph)
-      Fundamental.new( glyph_properties(glyph) )
-    end
-
-    def Particle.glyph_properties(glyph)
-      DEFINITIONS.detect { |p| p[:glyph] == glyph }
     end
   end
 end
