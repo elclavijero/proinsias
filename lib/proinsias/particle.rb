5ff04d35d2376d2f2640e26151b9724733963d69
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-08-13 11:40:59 +0100

    OK

--- lib/proinsias/particle.rb
+++ lib/proinsias/particle.rb
@@ -58,51 +58,6 @@ module Proinsias
       include Parous
       alias   arguments received
     end
-
-    class LParen
-      include Operator
-      include Disposition::Pessimistic
-
-      def self.create(*args)
-        new
-      end
-
-      def initialize(glyph='(')
-        @glyph = glyph
-        @capacity = 1
-        @strength = 0
-        @role = 'lparen'
-      end
-
-      def to_ast
-        arguments.first.to_ast
-      end
-
-      def receive(rparen)
-        if rparen.is_a?(Proinsias::Particle::RParen)
-          @glyph = '()'
-          @received = rparen.received
-        else
-          fail "Argument is not an RParen" unless rparen.is_a?(Proinsias::Particle::RParen)
-        end
-      end
-    end
-
-    class RParen
-      include Operator
-      include Disposition::Optimistic
-
-      def self.create(*args)
-        new
-      end
-
-      def initialize(glyph=')')
-        @glyph = glyph
-        @capacity = 1
-        @strength = 12
-        @role = 'rparen'
-      end
-    end
   end
 
   module Particle
@@ -260,5 +215,52 @@ module Proinsias
         disposition: 'Optimistic'
       },
     ]
+  end
+
+  module Particle
+    class LParen
+      include Operator
+      include Disposition::Pessimistic
+
+      def self.create(*args)
+        new
+      end
+
+      def initialize(glyph='(')
+        @glyph = glyph
+        @capacity = 1
+        @strength = 0
+        @role = 'lparen'
+      end
+
+      def to_ast
+        arguments.first.to_ast
+      end
+
+      def receive(rparen)
+        if rparen.is_a?(Proinsias::Particle::RParen)
+          @glyph = '()'
+          @received = rparen.received
+        else
+          fail "Argument is not an RParen" unless rparen.is_a?(Proinsias::Particle::RParen)
+        end
+      end
+    end
+
+    class RParen
+      include Operator
+      include Disposition::Optimistic
+
+      def self.create(*args)
+        new
+      end
+
+      def initialize(glyph=')')
+        @glyph = glyph
+        @capacity = 1
+        @strength = 12
+        @role = 'rparen'
+      end
+    end
   end
 end
