40506b57e183dd56dc1fa01d69e15100167e97b0
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-08-13 14:11:49 +0100

    Keyword agruments were inhibiting extension

--- lib/proinsias/particle_fundamental.rb
+++ lib/proinsias/particle_fundamental.rb
@@ -3,19 +3,14 @@ module Proinsias
     Fundamental = Class.new do
       include Receiver
 
-      attr_reader :glyph, :capacity, :strength, :role
+      attr_reader :glyph, :capacity, :strength, :role, :disposition
 
-      def initialize(
-        glyph:,
-        capacity:,
-        strength:,
-        role:,
-        disposition:
-      )
-        @glyph    = glyph
-        @capacity = capacity
-        @strength = strength
-        @role     = role
+      def initialize(definition)
+        @glyph    = definition.fetch(:glyph)
+        @capacity = definition.fetch(:capacity)
+        @strength = definition.fetch(:strength)
+        @role     = definition.fetch(:role)
+        @disposition = definition.fetch(:disposition)
 
         extend(
           Disposition.const_get(disposition)
