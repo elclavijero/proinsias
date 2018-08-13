bd69f362a0574c38be034bc63071de7eb4a46cc2
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-08-13 14:37:40 +0100

    Extract method

--- lib/proinsias/particle_fundamental.rb
+++ lib/proinsias/particle_fundamental.rb
@@ -8,26 +8,21 @@ module Proinsias
       attr_reader :ephemeral
 
       def initialize(definition)
-        @glyph    = definition.fetch(:glyph)
-        @capacity = definition.fetch(:capacity)
-        @strength = definition.fetch(:strength)
-        @role     = definition.fetch(:role)
+        @glyph       = definition.fetch(:glyph)
+        @capacity    = definition.fetch(:capacity)
+        @strength    = definition.fetch(:strength)
+        @role        = definition.fetch(:role)
         @disposition = definition.fetch(:disposition)
-        @sentinel = definition.fetch(:sentinel,nil)
-        @ephemeral = definition.fetch(:ephemeral,nil)
+        @sentinel    = definition.fetch(:sentinel,nil)
+        @ephemeral   = definition.fetch(:ephemeral,nil)
 
-        extend(
-          Disposition.const_get(disposition)
-        )
+        specialise
+      end
 
-        extend(
-          capacity > 0 ?
-            AST::Parous :
-            AST::Nonparous
-        )
-
+      def specialise
+        extend(Disposition.const_get(disposition))
+        extend(capacity > 0 ? AST::Parous : AST::Nonparous)
         extend(AST::Ephemeral) if ephemeral
-
         extend(Outfix) if sentinel
       end
     end
