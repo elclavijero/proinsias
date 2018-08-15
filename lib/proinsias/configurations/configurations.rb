4769d37ba25a80f2536f1811cb2a32523a0c7220
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-08-15 12:22:12 +0100

    OK

--- lib/proinsias/configurations/configurations.rb
+++ lib/proinsias/configurations/configurations.rb
@@ -4,8 +4,11 @@ require_relative './particle'
 
 module Proinsias
   module Configurations
-    def Configurations.get_controller(syntactic_form)
+    def Configurations.get_controller_for(syntactic_form)
       Controller::const_get(syntactic_form)
+    end
+
+    def Configurations.get_filter_for(language)
     end
   end
 end
