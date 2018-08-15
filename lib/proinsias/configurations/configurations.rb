687f430c67b5c3a37d98110c2b79efe9aca9ca9a
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-08-15 12:19:47 +0100

    OK

--- lib/proinsias/configurations/configurations.rb
+++ lib/proinsias/configurations/configurations.rb
@@ -5,13 +5,7 @@ require_relative './particle'
 module Proinsias
   module Configurations
     def Configurations.get_controller(form_name)
-      Controller.get(form_name)
-    end
-
-    module Controller
-      def Controller.get(form_name)
-        const_get(form_name)
-      end
+      Controller::const_get(form_name)
     end
   end
 end
