bdf3b85a1033eb148154c58d9df6d74345e52c58
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-08-16 13:36:53 +0100

    OK

--- lib/proinsias/parser/filter.rb
+++ lib/proinsias/parser/filter.rb
@@ -1,12 +1,57 @@
-require 'moory'
-
 module Proinsias
-  module Filter
+  class Filter < Proinsias::Logistic::Unit
+    attr_writer :consumer
+    attr_writer :quarantine
+    attr_reader :buffer
+
+    IGNORE = [' ', "\t", "\n"]
+    DEFAULT_CONSUMER = $stdout.method(:puts)
+    IGNORE_UNKNOWN   = proc { |c| warn "Warning! Ignoring unknown character: #{c}" }
+
     def Filter.create(consumer:, language:'Propositions')
-      Moory::Filter.new(
+      Proinsias::Filter.new(
         rules:    Configurations.get_filter_rules(language),
         consumer: consumer
       )
+    end
+  
+    def initialize(rules:, consumer:DEFAULT_CONSUMER, quarantine:IGNORE_UNKNOWN)
+      @buffer = ""
+      @consumer   = consumer
+      @quarantine = quarantine
+      super(rules: rules)
+    end
+  
+    def configure(rules)
+      super
+      repertoire.learn(name: 'produce', item: method(:produce))
+    end
+  
+    def issue(stimulus)
+      return if IGNORE.include?(stimulus)
+      
+      @buffer << stimulus
+      super
+    end
+
+    def produce(output)
+      consumer.call(@buffer)
+      @buffer = ""
+    end
+
+    private
+
+    def bad_stimulus(stimulus)
+      quarantine.call(stimulus)
+      @buffer = @buffer.slice(0..-2)
+    end
+
+    def consumer
+      @consumer || DEFAULT_CONSUMER
+    end
+
+    def quarantine
+      @quarantine || IGNORE_UNKNOWN
     end
   end
 end
--- spec/filter_spec.rb
+++ spec/filter_spec.rb
@@ -7,6 +7,145 @@ RSpec.describe Proinsias::Filter do
   end
 
   let(:the_consumer) do
+    spy('the consumer')
+  end
+
+  let(:the_quarantine) do
+    spy('the quarantine')
+  end
+
+  describe '#issue' do
+    describe "the Filter's state transitions and buffering behaviour" do
+    end
+    context 'in state "^"' do
+      before do
+        the_filter.state = '^'
+      end
+
+      context 'given the "p" stimulus,' do
+        context 'if a consumer has been assigned,' do
+          before do
+            the_filter.consumer = the_consumer
+          end
+
+          it 'the consumer will be called with "p"' do
+            the_filter.issue('p')
+  
+            expect(the_consumer).to have_received(:call).with('p')
+          end
+        end
+
+        context 'if a consumer is missing' do
+          before do
+            the_filter.consumer = nil
+          end
+
+          it 'DEFAULT_CONSUMER will be called with "p"' do
+            allow(Proinsias::Filter::DEFAULT_CONSUMER).to receive(:call)
+
+            the_filter.issue('p')
+  
+            expect(Proinsias::Filter::DEFAULT_CONSUMER).to have_received(:call).with('p')
+          end
+        end
+      end
+
+      context 'given a stimulus unknown to this state,' do
+        let(:unknown) { 'z' }
+
+        context 'if a quarantine has been assigned,' do
+          before do
+            the_filter.quarantine = the_quarantine
+          end
+
+          it 'the quarantine will be called with the unknown character' do
+            the_filter.issue(unknown)
+
+            expect(the_quarantine).to have_received(:call).with(unknown)
+          end
+        end
+
+        context 'if the quarantine is missing,' do
+          before do
+            the_filter.quarantine = nil
+          end
+
+          it 'IGNORE_UNKNOWN will be called with the unknown character' do
+            allow(Proinsias::Filter::IGNORE_UNKNOWN).to receive(:call)
+
+            the_filter.issue(unknown)
+  
+            expect(Proinsias::Filter::IGNORE_UNKNOWN).to have_received(:call).with(unknown)
+          end
+        end
+      end
+
+      describe 'issuing characters that are held by the buffer' do
+        context 'in state "^"' do
+          before do
+            the_filter.state = '^'
+          end
+
+          context 'after issuing an "t"' do
+            before do
+              the_filter.issue('t')
+            end
+
+            it 'the buffer will be "t"' do
+              expect(the_filter.buffer).to eq('t')
+            end
+
+            it 'the state will be "0"' do
+              expect(the_filter.state).to eq('0')
+            end
+          end
+
+          context 'after issuing "tr"' do
+            before do
+              the_filter.issue('t')
+              the_filter.issue('r')
+            end
+
+            it 'the buffer will be "tr"' do
+              expect(the_filter.buffer).to eq('tr')
+            end
+
+            it 'the state will be "1"' do
+              expect(the_filter.state).to eq('1')
+            end
+          end
+
+          context 'after issuing "true"' do
+            before do
+              the_filter.issue('t')
+              the_filter.issue('r')
+              the_filter.issue('u')
+              the_filter.issue('e')
+            end
+
+            it 'the buffer will be ""' do
+              expect(the_filter.buffer).to eq('')
+            end
+
+            it 'the state will be "^"' do
+              expect(the_filter.state).to eq('^')
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+RSpec.describe Proinsias::Filter do
+  let(:the_filter) do
+    Proinsias::Filter.create(
+      consumer: the_consumer,
+      language: 'Propositions'
+    )
+  end
+
+  let(:the_consumer) do
     spy("the consumer")
   end
 
