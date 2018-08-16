2329f71f22963bec1bba051dfe5c70931a2399b9
Adam W. Grant <adam_grnt@yahoo.co.uk>
2018-08-16 15:26:32 +0100

    OK

--- lib/proinsias/configurations/particle.rb
+++ lib/proinsias/configurations/particle.rb
@@ -134,4 +134,35 @@ module Proinsias
       ]
     end
   end
+
+  module Lambda
+    PARTICLE = [
+      # - VAR
+      {
+        glyph:       'x',
+        role:        'var',
+        capacity:    0,
+        strength:    0,
+        disposition: 'Pessimistic',
+      },
+
+      # - LAMBDA
+      {
+        glyph:       'λ',
+        role:        'lambda',
+        capacity:    1,
+        strength:    2,
+        disposition: 'Pessimistic',
+      },
+
+      # - .
+      {
+        glyph:       '.',
+        role:        '.',
+        capacity:    1,
+        strength:    1,
+        disposition: 'Pessimistic',
+      },
+    ]
+  end
 end
